db80a7577f366bd655d550f8f9158937aa494382
Modina Khusbu Priya <modinakhusbupriya@gmail.com>
2026-09-05 04:42:17 +0300

    add ObjectId

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 dotenv.config();
 
